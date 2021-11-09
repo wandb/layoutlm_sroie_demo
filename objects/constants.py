a56d58cc7c872e5f7d2791017e48b16739236923
Eric Bunch <eric@wandb.com>
2021-11-08 22:54:41 -0600

    change learning rate

--- objects/constants.py
+++ objects/constants.py
@@ -6,7 +6,7 @@ config = {
     "model_path": "model",
     "num_labels": 5,
     "pct_train": 0.8,
-    "learning_rate": 0.0001,
+    "learning_rate": 0.001,
     "batch_size_train": 4,
     "batch_size_test": 1,
     "epochs": 50,
